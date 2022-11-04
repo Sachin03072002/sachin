cbe0549b149e9ec83e8f787c325b5a96996c3e9c
sachin <sachinmondal720@gmail.com>
2022-11-04 23:55:13 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,3 @@
+// console.log("hii");
+const operation=require('./module.js');
+console.log(operation.add(1,2));
---
+++ module.js
@@ -0,0 +1,10 @@
+module.exports.add = function(a,b){
+  return a+b;
+}
+module.exports.sub = function(a,b){
+    return a-b;
+}
+//still works
+exports.multiply = function(a,b){
+    return a*b;
+}
